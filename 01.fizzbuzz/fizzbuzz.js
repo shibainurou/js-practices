5e7d1a703b30039135698a8fe34efb1616fbd736
shibainurou <shibainurou@gmail.com>
2024-02-25 19:04:14 +0900

    15のときにfizzbuzzにならない不具合修正

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -3,6 +3,6 @@
 for (let i = 1; i <= 20; i++) {
   let output = "";
   if (i % 3 === 0) output = "Fizz";
-  if (i % 5 === 0) output = "Buzz";
+  if (i % 5 === 0) output += "Buzz";
   console.log(output || i);
 }
