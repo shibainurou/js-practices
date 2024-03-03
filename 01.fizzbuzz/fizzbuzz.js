f8291c2238d802e01ec85bbd4ea483c585b9b093
shibainurou <shibainurou@gmail.com>
2024-03-03 17:46:07 +0900

    ifに{を追加

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 for (let i = 1; i <= 20; i++) {
   let output = "";
-  if (i % 3 === 0) output = "Fizz";
-  if (i % 5 === 0) output += "Buzz";
+  if (i % 3 === 0) {
+    output = "Fizz";
+  }
+  if (i % 5 === 0) {
+    output += "Buzz";
+  }
   console.log(output || i);
 }
