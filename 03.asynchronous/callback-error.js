aaea2d9eaee9ce0165c6e65f599bf2f65149e7d3
shibainurou <shibainurou@gmail.com>
2024-06-16 22:55:11 +0900

    callback版の非同期を共通化

--- 03.asynchronous/callback-error.js
+++ 03.asynchronous/callback-error.js
@@ -1,43 +1,10 @@
 #!/usr/bin/env node
 
-import sqlite3 from "sqlite3";
+import { main, Query } from "./callback-common.js";
 
-const db = new sqlite3.Database(":memory:", function () {
-  onCreatedDatabaseEvent(db);
-});
-
-function onCreatedDatabaseEvent(db) {
-  db.run(
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE);",
-    function () {
-      onCreatedTableEvent(db);
-    },
-  );
-}
-
-function onCreatedTableEvent(db) {
-  const stmt = db.prepare("INSERT INTO books (id, title) VALUES (?, ?)");
-  stmt.run(`a`, `title`, function (err) {
-    if (err) {
-      console.error(err.message);
-    }
-    onInsertedRowEvent(db, stmt);
-  });
-}
-
-function onInsertedRowEvent(db, stmt) {
-  stmt.finalize(function () {
-    onCommitdEvent(db);
-  });
-}
-
-function onCommitdEvent(db) {
-  db.get("SELECT ids, titles FROM books", function (err) {
-    if (err) {
-      console.error(err.message);
-    }
-    db.run("DROP TABLE books", function () {
-      db.close();
-    });
-  });
-}
+main(
+  new Query(
+    "INSERT INTO books (id, title) VALUES (?, ?)",
+    "SELECT ids, titles FROM books",
+  ),
+);
