0c56f505af577e1e2b6db4159f66f4e94c962d57
shibainurou <shibainurou@gmail.com>
2024-10-27 21:33:23 +0900

    処理個別のメソッドを削除して、runとgetRecordのみでクエリを実行するように修正

--- 03.asynchronous/promise-common.js
+++ 03.asynchronous/promise-common.js
@@ -8,53 +8,10 @@ export function openDatabase() {
   });
 }
 
-export function createTable(db) {
-  return run(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
-}
-
-export function insertRow(db, query, parames = []) {
-  return run(db, query, parames);
-}
-
-export function selectRow(db, query) {
-  return getRecord(db, query);
-}
-
-export function dropTable(db) {
-  return run(db, "DROP TABLE books");
-}
-
 export function closeDatabase(db) {
   return new Promise((resolve) => {
     db.close(() => {
       resolve();
-    });
-  });
-}
-
-function run(db, query, parames = []) {
-  return new Promise((resolve, reject) => {
-    db.run(query, parames, function (err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(this);
-      }
-    });
-  });
-}
-
-function getRecord(db, query) {
-  return new Promise((resolve, reject) => {
-    db.get(query, (err, row) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(row);
-      }
     });
   });
 }
--- 03.asynchronous/promise-success.js
+++ 03.asynchronous/promise-success.js
@@ -1,13 +1,6 @@
 #!/usr/bin/env node
 
-import {
-  openDatabase,
-  createTable,
-  insertRow,
-  selectRow,
-  dropTable,
-  closeDatabase,
-} from "./promise-common.js";
+import { openDatabase, closeDatabase } from "./promise-common.js";
 
 var db = null;
 
@@ -17,18 +10,43 @@ function main() {
   openDatabase()
     .then((database) => {
       db = database;
-      return createTable(db);
+      return run(
+        db,
+        "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+      );
     })
-    .then(() =>
-      insertRow(db, "INSERT INTO books (title) VALUES (?)", ["book title"]),
-    )
+    .then(() => run(db, "INSERT INTO books (title) VALUES (?)", ["book title"]))
     .then((result) => {
       console.log(`id: ${result.lastID}`);
-      return selectRow(db, "SELECT id, title FROM books");
+      return getRecord(db, "SELECT id, title FROM books");
     })
     .then((row) => {
       console.log("id: " + row.id + ", title: " + row.title);
-      return dropTable(db);
+      return run(db, "DROP TABLE books");
     })
     .then(() => closeDatabase);
+}
+
+function run(db, query, parames = []) {
+  return new Promise((resolve, reject) => {
+    db.run(query, parames, function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(this);
+      }
+    });
+  });
+}
+
+function getRecord(db, query) {
+  return new Promise((resolve, reject) => {
+    db.get(query, (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
+      }
+    });
+  });
 }
