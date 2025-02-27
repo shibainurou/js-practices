aa6a763abef1d503f916345ec490842fc99d09d8
shibainurou <shibainurou@gmail.com>
2025-02-28 06:34:02 +0900

    callbackで書いた非同期処理の即時実行関数式を削除した

--- 03.asynchronous/callback-error.js
+++ 03.asynchronous/callback-error.js
@@ -2,33 +2,31 @@
 
 import sqlite3 from "sqlite3";
 
-(function () {
-  const db = new sqlite3.Database(":memory:", function () {
-    db.run(
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE);",
-      function () {
-        db.run(
-          "INSERT INTO books (id, title) VALUES (?, ?)",
-          ["a", "book title"],
-          function (err) {
+const db = new sqlite3.Database(":memory:", function () {
+  db.run(
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE);",
+    function () {
+      db.run(
+        "INSERT INTO books (id, title) VALUES (?, ?)",
+        ["a", "book title"],
+        function (err) {
+          if (err) {
+            console.error(err.message);
+          } else {
+            console.log(`id: ${this.lastID}`);
+          }
+          db.get("SELECT ids, title FROM books", function (err, row) {
             if (err) {
               console.error(err.message);
             } else {
-              console.log(`id: ${this.lastID}`);
+              console.log(`id: ${row.id}, title: ${row.title}`);
             }
-            db.get("SELECT ids, title FROM books", function (err, row) {
-              if (err) {
-                console.error(err.message);
-              } else {
-                console.log(`id: ${row.id}, title: ${row.title}`);
-              }
-              db.run("DROP TABLE books", function () {
-                db.close();
-              });
+            db.run("DROP TABLE books", function () {
+              db.close();
             });
-          },
-        );
-      },
-    );
-  });
-})();
+          });
+        },
+      );
+    },
+  );
+});
