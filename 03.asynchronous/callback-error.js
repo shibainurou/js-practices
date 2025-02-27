48fc8659f6d9fe85c1f16754f16cf5a0c26ec9bb
shibainurou <shibainurou@gmail.com>
2025-02-28 06:57:51 +0900

    関数内でその関数の this を参照する必要がない箇所をfunction式からアロー関数に変更した

--- 03.asynchronous/callback-error.js
+++ 03.asynchronous/callback-error.js
@@ -2,10 +2,10 @@
 
 import sqlite3 from "sqlite3";
 
-const db = new sqlite3.Database(":memory:", function () {
+const db = new sqlite3.Database(":memory:", () => {
   db.run(
     "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE);",
-    function () {
+    () => {
       db.run(
         "INSERT INTO books (id, title) VALUES (?, ?)",
         ["a", "book title"],
@@ -15,13 +15,13 @@ const db = new sqlite3.Database(":memory:", function () {
           } else {
             console.log(`id: ${this.lastID}`);
           }
-          db.get("SELECT ids, title FROM books", function (err, row) {
+          db.get("SELECT ids, title FROM books", (err, row) => {
             if (err) {
               console.error(err.message);
             } else {
               console.log(`id: ${row.id}, title: ${row.title}`);
             }
-            db.run("DROP TABLE books", function () {
+            db.run("DROP TABLE books", () => {
               db.close();
             });
           });
