9e55eb6def355c7ddbad90f30f2d08f06902971f
shibainurou <shibainurou@gmail.com>
2025-02-28 07:02:29 +0900

    CREATE TABLEのSQLから';'を削除し、他のクエリと合わせた

--- 03.asynchronous/callback-error.js
+++ 03.asynchronous/callback-error.js
@@ -4,7 +4,7 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:", () => {
   db.run(
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE);",
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
     () => {
       db.run(
         "INSERT INTO books (id, title) VALUES (?, ?)",
