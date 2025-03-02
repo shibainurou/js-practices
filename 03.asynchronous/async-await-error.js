977a5d9ce32fdc9250ccb47db9988695c95d738e
shibainurou <shibainurou@gmail.com>
2025-03-02 11:21:01 +0900

    モジュールの最上位なのでasync functionを削除した

--- 03.asynchronous/async-await-error.js
+++ 03.asynchronous/async-await-error.js
@@ -3,39 +3,34 @@
 import { run, get, close, open } from "./sqlite3-wrapper.js";
 
 const databaseName = "memory";
-let db;
 
-(async function () {
-  db = await open(databaseName);
+const db = await open(databaseName);
+await run(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
-  await run(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
-
-  try {
-    const result = await run(
-      db,
-      "INSERT INTO books (id, title) VALUES (?, ?)",
-      ["a", "book title"],
-    );
-    console.log(`id: ${result.lastID}`);
-  } catch (error) {
-    if (error.code === "SQLITE_MISMATCH") {
-      console.error(error.message);
-    }
+try {
+  const result = await run(db, "INSERT INTO books (id, title) VALUES (?, ?)", [
+    "a",
+    "book title",
+  ]);
+  console.log(`id: ${result.lastID}`);
+} catch (error) {
+  if (error.code === "SQLITE_MISMATCH") {
+    console.error(error.message);
   }
+}
 
-  try {
-    const row = await get(db, "SELECT ids, title FROM books");
-    console.log(`id: ${row.id}, title: ${row.title}`);
-  } catch (error) {
-    if (error.code === "SQLITE_ERROR") {
-      console.error(error.message);
-    }
+try {
+  const row = await get(db, "SELECT ids, title FROM books");
+  console.log(`id: ${row.id}, title: ${row.title}`);
+} catch (error) {
+  if (error.code === "SQLITE_ERROR") {
+    console.error(error.message);
   }
+}
 
-  await run(db, "DROP TABLE books");
+await run(db, "DROP TABLE books");
 
-  await close(db);
-})();
+await close(db);
--- 03.asynchronous/async-await-success.js
+++ 03.asynchronous/async-await-success.js
@@ -3,25 +3,22 @@
 import { run, get, close, open } from "./sqlite3-wrapper.js";
 
 const databaseName = "memory";
-let db;
 
-(async function () {
-  db = await open(databaseName);
+const db = await open(databaseName);
 
-  await run(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
+await run(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
-  const result = await run(db, "INSERT INTO books (title) VALUES (?)", [
-    "book title",
-  ]);
-  console.log(`id: ${result.lastID}`);
+const result = await run(db, "INSERT INTO books (title) VALUES (?)", [
+  "book title",
+]);
+console.log(`id: ${result.lastID}`);
 
-  const row = await get(db, "SELECT id, title FROM books");
-  console.log(`id: ${row.id}, title: ${row.title}`);
+const row = await get(db, "SELECT id, title FROM books");
+console.log(`id: ${row.id}, title: ${row.title}`);
 
-  await run(db, "DROP TABLE books");
+await run(db, "DROP TABLE books");
 
-  await close(db);
-})();
+await close(db);
