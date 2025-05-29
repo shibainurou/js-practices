66f5305de9dfddd76491b8305e039761f2edcb52
shibainurou <shibainurou@gmail.com>
2025-05-30 07:39:53 +0900

    メモアプリ完成

---
+++ 04.class/MemoRepository.js
@@ -0,0 +1,42 @@
+import { Database } from "./database.js";
+import { MemoEntity } from "./MemoEntity.js";
+
+export class MemoRepository {
+  async initialize() {
+    this.database = new Database();
+    await this.database.open();
+    await this.database.createTable();
+  }
+
+  async findAll() {
+    const memos = await this.database.run(
+      "SELECT id, title, content FROM memos",
+    );
+    return memos.map(
+      (memo) => new MemoEntity(memo.id, memo.title, memo.content),
+    );
+  }
+
+  async findById(id) {
+    const [memo] = await this.database.run(
+      "SELECT id, title, content FROM memos WHERE id = ?",
+      [id],
+    );
+    return new MemoEntity(memo.id, memo.title, memo.content);
+  }
+
+  async insert(memo) {
+    await this.database.run(
+      "INSERT INTO memos (title, content) VALUES (?, ?)",
+      [memo.title, memo.content],
+    );
+  }
+
+  async delete(id) {
+    await this.database.run("DELETE FROM memos WHERE id = ?", [id]);
+  }
+
+  async close() {
+    await this.database.close();
+  }
+}
